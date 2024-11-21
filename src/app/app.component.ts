792a4e183fe955bb6599c988864c007326c8befc
Dmytro Doronin <138795636+Dmytro-Doronin@users.noreply.github.com>
2024-11-21 22:00:27 +0100

    start project and add angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbar, MatToolbarModule} from '@angular/material/toolbar';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ MatToolbar, MatToolbarModule, MatButtonModule],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
