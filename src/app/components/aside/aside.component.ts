69abc2127ffab1142dfe06d2e3178c4122b9c243
Dmytro Doronin <138795636+Dmytro-Doronin@users.noreply.github.com>
2024-11-22 17:40:58 +0100

    nav list

--- src/app/components/aside/aside.component.ts
+++ src/app/components/aside/aside.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListItem, MatNavList} from '@angular/material/list';
 
 @Component({
   selector: 'app-aside',
-  imports: [MatSidenavModule],
+  imports: [MatSidenavModule, MatIconModule, MatNavList, MatListItem],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   templateUrl: './aside.component.html',
-  styleUrl: './aside.component.scss'
+  styleUrls: ['./aside.component.scss'],
 })
 export class AsideComponent {
 
